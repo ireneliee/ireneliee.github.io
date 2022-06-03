d0d13f8e5690bd41a495e616ec771aab6ecb3b38
unknown <irenelie1412@gmail.com>
2022-06-04 01:43:30 +0800

    adding routing

---
+++ src/pages/Bio.tsx
@@ -0,0 +1,16 @@
+import {IonButton, IonContent, IonHeader, IonPage, IonRouterOutlet, IonTitle, IonToolbar} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+import MovingTitle from "../components/MovingTitle";
+import Coffee from "../components/Coffee";
+import {Route} from "react-router-dom";
+
+const Bio: React.FC = () => {
+    return (
+        <IonPage>
+
+        </IonPage>
+    );
+};
+
+export default Bio;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,12 +1,17 @@
-import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import {IonButton, IonContent, IonHeader, IonPage, IonRouterOutlet, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import MovingTitle from "../components/MovingTitle";
 import Coffee from "../components/Coffee";
+import {Route} from "react-router-dom";
+import Bio from "./Bio";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
+        <IonRouterOutlet>
+            <Route path="/tab1/bio" component={Bio} />
+        </IonRouterOutlet>
 
       <IonContent fullscreen class = "tab-one-fullscreen">
           <div className = "animation-container-homepage">
@@ -18,7 +23,7 @@ const Tab1: React.FC = () => {
               </div>
           </div>
           <div className = "button-menu">
-              <IonButton color="light" className = "button-choice">Bio</IonButton>
+              <IonButton color="light" className = "button-choice" href = "Bio">Bio</IonButton>
               <IonButton color="medium" className = "button-choice">Notable Project</IonButton>
               <IonButton color="dark" className = "button-choice">Social Media</IonButton>
           </div>
